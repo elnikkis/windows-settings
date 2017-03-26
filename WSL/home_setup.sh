ab6afa643e3b5b9622a70177ee3c541e16f1c3d7
elnikkis <noahluces@gmail.com>
2017-03-26 23:57:45 +0900

    windows-settingsのcloneを追加した

--- WSL/home_setup.sh
+++ WSL/home_setup.sh
@@ -2,12 +2,19 @@
 
 # HOMEを整える
 
+mkdir -p ~/repos
+
 # dotfilesの取得・配置
 if [ ! -e ~/repos/dotfiles ] ; then
-    mkdir -p ~/repos
     cd ~/repos
     git clone https://github.com/elnikkis/dotfiles.git
     cd ~/repos/dotfiles
     rm ~/.bashrc
     yes n | python makelink.py
+fi
+
+if [ ! -e ~/repos/windows-settings ] ; then
+    cd ~/repos
+    git clone https://github.com/elnikkis/windows-settings.git
+    cp ~/repos/windows-settings/WSL/start_sshd.sh ~/
 fi
